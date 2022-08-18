d8abc5cbe2e59cd57b062cb6f5132e14bff1a8db
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-18 11:08:53 +0700

    menambahakan softdelte book di modelnya

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Book extends Model
 {
     use HasFactory;
-
+    use SoftDeletes;
     public function orders()
     {
         return $this->belongsToMany("App\Models\Order");
