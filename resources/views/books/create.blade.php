71edf7af670c3088063c1b3e36e6e6f6e3eb7949
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-18 10:22:25 +0700

    menambahakan cdnjs

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.global')
+@section('footer-scripts')
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+@endsection
 @section('title') Create book @endsection
 
 @section('content')
