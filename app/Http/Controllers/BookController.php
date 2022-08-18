dc92e87e3afac9bba3d3f972a817fda35a2993ed
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-18 11:28:45 +0700

    mengganti query pencarian agar first

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -16,11 +16,12 @@ class BookController extends Controller
         // $books = \App\Models\Book::with('categories')->paginate(10);
 
         $status = $request->get('status');
+        $keyword = $request->get('keyword') ? $request->get('keyword') : '';
 
         if ($status) {
-            $books = \App\Models\Book::with('categories')->where('status', strtoupper($status))->paginate(10);
+            $books = \App\Models\Book::with('categories')->where('title', "LIKE", "%$keyword%")->where('status', strtoupper($status))->paginate(10);
         } else {
-            $books = \App\Models\Book::with('categories')->paginate(10);
+            $books = \App\Models\Book::with('categories')->where("title", "LIKE", "%$keyword%")->paginate(10);
         }
 
         return view('books.index', ['books' => $books]);
