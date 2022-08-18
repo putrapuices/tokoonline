a51c5801251c8f39a88a998408c3cde7109f92c6
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-18 11:12:50 +0700

    menambahakna function trash

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -152,4 +152,11 @@ class BookController extends Controller
 
         return redirect()->route('books.index')->with('status', 'Book moved to trash');
     }
+
+    public function trash()
+    {
+        $books = \App\Models\Book::onlyTrashed()->paginate(10);
+
+        return view('books.trash', ['books' => $books]);
+    }
 }
